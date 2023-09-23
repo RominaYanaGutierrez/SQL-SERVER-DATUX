0650ba88beb1f091188891c415317e34a4424085
RominaYanaGutierrez <145816471+RominaYanaGutierrez@users.noreply.github.com>
2023-09-22 21:13:55 -0500

    Add files via upload

---
+++ Problema-1.py
@@ -0,0 +1,8 @@
+'''
+Escribir un programa que solicite su nombre de usuario por consola y después de que el usuario lo
+introduzca muestre por pantalla la cadena “¡Hola <nombre>!”, donde <nombre> es el nombre que el
+usuario haya introducido
+'''
+
+nombre = input("Bienvenido, introduce tu nombre: ")
+print("¡Hola " + nombre + "!")
